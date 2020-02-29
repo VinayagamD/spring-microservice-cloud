c93890a0a4760a445d6d13ed40176cc977a9a7ec
VinayagamD <vinayagam.d.ganesh@gmail.com>
2020-02-29 09:08:16 +0530

    eureka naming server added

---
+++ currency-naming-server/src/main/java/com/vinay/currencynamingserver/CurrencyNamingServerApplication.java
@@ -0,0 +1,15 @@
+package com.vinay.currencynamingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class CurrencyNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyNamingServerApplication.class, args);
+	}
+
+}
